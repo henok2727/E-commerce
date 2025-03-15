25938ac468d931b695f2e3868d20a64a2772da24
henok2727 <henokketema27@gmail.com>
2025-03-15 16:04:55 -0700

    Revert Finished
    This reverts commit 276e347b60a844b6e638fbbe7c429e6929cd8aaf.

--- frontend/src/redux/api/productApiSlice.js
+++ frontend/src/redux/api/productApiSlice.js
@@ -1,5 +1,5 @@
-import { PRODUCT_URL, UPLOAD_URL } from "../constants";
-import { apiSlice } from "./apiSlice";
+import {PRODUCT_URL, UPLOAD_URL} from '../constants'
+import {apiSlice} from './apiSlice'
 
 export const productApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -7,11 +7,12 @@ export const productApiSlice = apiSlice.injectEndpoints({
       query: ({ keyword }) => ({
         url: `${PRODUCT_URL}`,
         params: { keyword },
-      }),
-      keepUnusedDataFor: 5,
-      providesTags: ["Products"],
-    }),
+      }),  
 
+      keepUnusedDataFor: 5,
+      providesTags: ["Product"], 
+    }),
+    
     getProductById: builder.query({
       query: (productId) => `${PRODUCT_URL}/${productId}`,
       providesTags: (result, error, productId) => [
@@ -20,12 +21,12 @@ export const productApiSlice = apiSlice.injectEndpoints({
     }),
 
     allProducts: builder.query({
-      query: () => `${PRODUCT_URL}/allProducts`,
+      query: () => `${PRODUCT_URL}/allProducts`,      
     }),
 
     getProductDetails: builder.query({
       query: (productId) => ({
-        url: `${PRODUCT_URL}/${productId}`,
+        url: `${PRODUCT_URL}/ ${productId}`
       }),
       keepUnusedDataFor: 5,
     }),
@@ -33,75 +34,66 @@ export const productApiSlice = apiSlice.injectEndpoints({
     createProduct: builder.mutation({
       query: (productData) => ({
         url: `${PRODUCT_URL}`,
-        method: "POST",
+        method: 'POST',
         body: productData,
       }),
       invalidatesTags: ["Product"],
     }),
 
-    updateProduct: builder.mutation({
-      query: ({ productId, formData }) => ({
-        url: `${PRODUCT_URL}/${productId}`,
-        method: "PUT",
-        body: formData,
+      updateProduct: builder.mutation({
+        query: ({productId, formData}) => ({
+          url: `${PRODUCT_URL}/${productId}`,
+          method: 'PUT',
+          body: formData,
+        })
       }),
-    }),
 
-    uploadProductImage: builder.mutation({
-      query: (data) => ({
-        url: `${UPLOAD_URL}`,
-        method: "POST",
-        body: data,
+      uploadProductImage: builder.mutation({
+        query: (data) => ({
+          url: `${UPLOAD_URL}`,
+          method: "POST",
+          body: data
+        }),
       }),
-    }),
 
-    deleteProduct: builder.mutation({
-      query: (productId) => ({
-        url: `${PRODUCT_URL}/${productId}`,
-        method: "DELETE",
+      deleteProduct: builder.mutation({
+        query: (productId) => ({
+          url: `${PRODUCT_URL}/${productId}`,
+          method: "DELETE",
+        }),
+        providesTags: ["Product"],
       }),
-      providesTags: ["Product"],
-    }),
 
-    createReview: builder.mutation({
-      query: (data) => ({
-        url: `${PRODUCT_URL}/${data.productId}/reviews`,
-        method: "POST",
-        body: data,
+      createReview: builder.mutation({
+        query: (data) => ({
+          url: `${PRODUCT_URL}/${productId}`,
+          method: "POST",
+          body: data
+        }),
       }),
-    }),
-
-    getTopProducts: builder.query({
-      query: () => `${PRODUCT_URL}/top`,
-      keepUnusedDataFor: 5,
-    }),
-
-    getNewProducts: builder.query({
-      query: () => `${PRODUCT_URL}/new`,
-      keepUnusedDataFor: 5,
-    }),
-
-    getFilteredProducts: builder.query({
-      query: ({ checked, radio }) => ({
-        url: `${PRODUCT_URL}/filtered-products`,
-        method: "POST",
-        body: { checked, radio },
+     
+      getTopProducts: builder.query({
+        query: () => `${PRODUCT_URL}/top`,
+        keepUnusedDataFor: 5,
       }),
-    }),
+      
+      getNewProducts: builder.query({
+        query: () => `${PRODUCT_URL}/new`,
+        keepUnusedDataFor: 5,
+      }),
   }),
 });
 
 export const {
-  useGetProductByIdQuery,
-  useGetProductsQuery,
-  useGetProductDetailsQuery,
-  useAllProductsQuery,
-  useCreateProductMutation,
-  useUpdateProductMutation,
-  useDeleteProductMutation,
-  useCreateReviewMutation,
-  useGetTopProductsQuery,
-  useGetNewProductsQuery,
-  useUploadProductImageMutation,
-  useGetFilteredProductsQuery,
-} = productApiSlice;
+    useGetProductByIdQuery,
+    useGetProductsQuery,
+    useGetProductDetailsQuery,
+    useAllProductsQuery,
+    useCreateProductMutation,
+    useUpdateProductMutation,
+    useDeleteProductMutation,
+    useCreateReviewMutation,
+    useGetTopProductsQuery,
+    useGetNewProductsQuery,
+    useUploadProductImageMutation,
+} = productApiSlice
